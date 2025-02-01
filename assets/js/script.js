fe2324571bcfb91925a476ca52b19a01ab29a7ad
Musaib03 <wanimusaib520@gmail.com>
2025-02-01 23:14:11 +0530

    js and styles modified for slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     var splide = new Splide('#splide', {
-      type: 'loop',
       autoplay: true,
       interval: 3000,
       perPage: 5,
       perMove: 1,
       gap: '1rem',
       breakpoints: {
-        768: {
-          perPage: 1,
-        },
         1024: {
           perPage: 3,
+        },
+        768: {
+          perPage: 1,
         },
       },
     });
